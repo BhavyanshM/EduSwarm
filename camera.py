055c83c4c7fc183f33384bbed574d609aa4a62ea
BhavyanshM <bhavyanshmishra@gmail.com>
2018-03-13 12:45:06 -0500

    Add the cropping feature to the video stream on openCV script

--- camera.py
+++ camera.py
@@ -4,13 +4,14 @@ import numpy as np
 cap = cv2.VideoCapture(1);
 
 while True:
+	cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 	ret, frame = cap.read()
-
+	# print(frame.shape)
+	frame = frame[100:400, 100:600]
 	cv2.imshow('frame', frame)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
-		b
-		reak
+		break
 
 cap.release()
 cv2.destroyAllWindows()
